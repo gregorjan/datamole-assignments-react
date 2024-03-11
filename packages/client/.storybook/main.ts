1b6fd6bd38c243a25f438378c26f6da06cb42892
gregorjan <jan.gregor@tuta.io>
2024-03-11 14:25:45 +0100

    sb-3 pseudostates

--- packages/client/.storybook/main.ts
+++ packages/client/.storybook/main.ts
@@ -1,6 +1,15 @@
-module.exports = {
+import { type StorybookConfig } from "@storybook/react-vite";
+import { withoutVitePlugins } from "@storybook/builder-vite";
+import { mergeConfig } from "vite";
+
+const config: StorybookConfig = {
     stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "storybook-addon-pseudo-states",
+    ],
     framework: {
         name: "@storybook/react-vite",
         options: {},
@@ -8,4 +17,15 @@ module.exports = {
     docs: {
         autodocs: true,
     },
+    async viteFinal(config) {
+        config.plugins = await withoutVitePlugins(config.plugins, ["vite:dts"]);
+
+        return mergeConfig(config, {
+            optimizeDeps: {
+                include: ["@radix-ui/colors", "styled-normalize", "styled-reset"],
+            },
+        });
+    },
 };
+
+export default config;
