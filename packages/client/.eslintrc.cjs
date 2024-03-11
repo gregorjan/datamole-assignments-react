8cf89e1c90ffee70e1a960e24c25759d88db9298
gregorjan <jan.gregor@tuta.io>
2024-03-11 11:51:33 +0100

    fixed eslint warnings

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     parser: "@typescript-eslint/parser",
     parserOptions: {
-        project: "tsconfig.json",
+        project: ["tsconfig.json", "tsconfig.node.json"],
         sourceType: "module",
         tsconfigRootDir: __dirname,
     },
@@ -18,7 +18,7 @@ module.exports = {
         node: true,
         jest: true,
     },
-    ignorePatterns: [".eslintrc.js"],
+    ignorePatterns: [".eslintrc.cjs"],
     rules: {
         "@typescript-eslint/interface-name-prefix": "off",
         "@typescript-eslint/explicit-function-return-type": "off",
